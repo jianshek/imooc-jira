d6e18ce7636a01c8a96d33d38d684fcff781ea33
flyingish <948698823@qq.com>
2021-08-23 15:55:07 +0800

    添加emotion(css-in-js)

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent } from "react";
 import { useAuth } from "context/auth-context";
-import { Button, Form, Input } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app/index";
 
 export const RegisterScreen = () => {
     const { register } = useAuth();
@@ -25,9 +26,9 @@ export const RegisterScreen = () => {
                 <Input placeholder={"密码"} type="password" id={"password"} />
             </Form.Item>
             <Form.Item>
-                <Button htmlType={"submit"} type={"primary"}>
+                <LongButton htmlType={"submit"} type={"primary"}>
                     注册
-                </Button>
+                </LongButton>
             </Form.Item>
         </Form>
     );
