63f0c5229a768ff638dfc7bc283efde5c1da9da1
flyingish <948698823@qq.com>
2021-09-22 16:27:26 +0800

    添加看板图标

--- src/screens/kanban/util.ts
+++ src/screens/kanban/util.ts
@@ -3,6 +3,7 @@ import { useProject } from "utils/project";
 import { useKanbans } from "utils/kanban";
 import { useTasks } from "utils/task";
 
+//获取url中的id
 export const useProjectIdInUrl = () => {
     const { pathname } = useLocation();
     const id = pathname.match(/projects\/(\d+)/)?.[1];
@@ -11,8 +12,10 @@ export const useProjectIdInUrl = () => {
 
 export const useProjectInUrl = () => useProject(useProjectIdInUrl());
 
-export const useKanbansInProject = () =>
-    useKanbans({ projectId: useProjectIdInUrl() });
+export const useKanbanSearchParams = () => ({ projectId: useProjectIdInUrl() });
 
-export const useTasksInProject = () =>
-    useTasks({ projectId: useProjectIdInUrl() });
+export const useKanbansQueryKey = () => ["kanbans", useKanbanSearchParams()];
+
+export const useTasksSearchParams = () => ({ projectId: useProjectIdInUrl() });
+
+export const useTasksQueryKey = () => ["tasks", useTasksSearchParams()];
